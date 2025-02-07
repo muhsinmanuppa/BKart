7d0828bdea5683984daefd0b38ee5bd9874b7ac4
muhsinmanuppa <muhsinmanuppa@gmail.com>
2025-02-07 08:59:30 +0530

    Updated vite.config.js for Vercel deployment

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,10 +4,15 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   build: {
-    outDir: "dist",  // Vercel expects the output here
+    outDir: "dist", // Vercel expects the output here
     emptyOutDir: true,
   },
   server: {
     port: 3000, // Local development server port
-  }
+    host: true, // Allows access from network
+  },
+  preview: {
+    port: 8080, // Preview mode on Vercel
+    host: true,
+  },
 });
